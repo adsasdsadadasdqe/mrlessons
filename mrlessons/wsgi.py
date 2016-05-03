224e5d96960373d5659b8f44d17ce46b69510e6d
toytanik <toytanik@github.com>
2016-05-03 15:21:48 +0300

    Marmara Pdf first commit

---
+++ mrlessons/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mrlessons project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mrlessons.settings")
+
+application = get_wsgi_application()
